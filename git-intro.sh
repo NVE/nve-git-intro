0a31399e327e0c2e471166ca112061276abece7b
Jan Magnusson <jmg@nve.no>
2016-09-26 14:19:28 +0200

    Update git-intro.sh

--- git-intro.sh
+++ git-intro.sh
@@ -1,42 +1,54 @@
-#!/bin/bash
+# Before starting, we need to provide some credentials.
+# Make sure they match your credential on GitHub if you want to use it later on.
+# We only need to do this step once, so skip if you have already setup git correctly.
 
-# Exploring the very basics of *git*.
-# We start with a small project.
+git config --global user.email "change to your email!!!"
+git config --global user.name "change to the username of your Github account!!!"
+
+# Check that your info is correct.
+
+git config --list
+
+# Exploring the basics of Git using a with a small project.
+
 mkdir my_local_repo
 cd my_local_repo
-echo "# Header" > README.md
-echo "" >>README.md
-echo "some text" >> README.md
+touch README.md
+
+# Edit the readme file using some text editor
 
 # Now we initialize our *git* repository and check its status.
+
 git init
 git status
 
-#For our first commit we need to tell *git* which files we want to track.
+# For our first commit we need to tell *git* which files we want to track.
+# With git add we tell which files git will take a snapshot of.
+
 git add *.md
 git status
 
-# Ooops! we need to provide some credentials. Make sure they match your credential on *GitHub* if you want to use it later on. We need to do this only once.
-#git config --global user.email "my email"
-#git config --global user.name "My name"
+# Now we can commit our project and check the status again.
+# Always make the commit-message somewhat meaningful, then it is easier to track your changes.
+# A commit takes a snapshot of the added files, and stored them.
 
-# Git can store these credentials in several places: 1. for your system, 2. for your user, 3. for your repository.
-# We are normally only concerned with setting for 2. (using the *--global* option) and 3..
-# On Windows systems, Git looks for the .gitconfig file in the $HOME directory (typically *C:\Users\$USER*).
-# You can check your settings by typing
-git config --list
-
-# Now we can commit our project and check the status again. Always make the commit-message somewhat meaningful, then it is easier to track your changes.
 git commit -m "My first local commit"
 git status
 
 # There is also a log.
+
 git log
 
+# And a short version of the log.
+
+git log --oneline
+
 # Now, let's do some more changes, add some files, and commit often.
+
 echo "and more text" >> README.md # some minor edits
 
 # To see what you’ve changed but not yet staged, type git diff with no other arguments.
+
 git diff
 git commit -a -m "My second commit" # (without staging like *git add* first)
 git log
@@ -45,13 +57,12 @@ git add hello.py
 git commit -m "added python file"
 git status
 
-#Assume we did not want the hello.py file in our repo.
+# Assume we did not want the hello.py file in our repo.
+
 rm hello.py
 git rm hello.py
 git commit -m "removed python file"
 git log
-
-#If we wanted to keep *hello.py* in our work folder, but do not track it we could have used *git rm --cached hello.py*  and added it to our *.gitignore* file.
 
 ### Branching
 
